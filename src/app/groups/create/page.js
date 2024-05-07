cd3a34f0c42f00b55dc3567f9d505a09afc1c0ae
Ampi-Z <azanardi@fi.uba.ar>
2024-05-07 13:06:14 -0300

    Create group

--- src/app/groups/create/page.js
+++ src/app/groups/create/page.js
@@ -1,5 +1,43 @@
-export default function CreateGroup() {
+'use client'
+
+import React, { useState } from 'react';
+import { Button, TextField, Grid, Typography, Card } from '@mui/material';
+
+function CreateGroup() {
+  const [groupName, setGroupName] = useState('');
+
+  const handleNameChange = (event) => setGroupName(event.target.value);
+
+  const Submit = (event) => {
+    event.preventDefault();
+    console.log('Name:', groupName);
+  };
+
+
   return (
-    <h1>GROUP CREATE</h1>
-  )
+    <Grid container alignItems="center" justifyContent="center" style={{ height: '100vh' }}>
+        <Card variant="outlined" sx={{ p: 4}}>
+        <Typography variant="h6" component="h2" gutterBottom>
+          Nuevo Grupo
+        </Typography>
+        <form onSubmit={Submit}>
+          <TextField
+            label="Nombre"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={groupName}
+            onChange={handleNameChange}
+          />
+        
+          <Button type="submit" variant="contained" color="primary">
+            Crear Grupo
+          </Button>
+
+        </form>
+        </Card>
+      </Grid>
+  );
 }
+
+export default CreateGroup;
