37814f87c7aea9b98d376ac36e98833c3fadc495
Joaquin Ordoñez <joacoordo@gmail.com>
2024-05-12 11:29:54 -0300

    notificactions can be read now

--- src/app/services/notification.js
+++ src/app/services/notification.js
@@ -3,8 +3,17 @@ import axios from "./axios";
 export const getNotifications = async () => {
   try{
     const response = await axios.get('/notifications')
+    console.log(response.data);
     return response.data
   }catch (error) {
+    throw error;
+  }
+}
+
+export const readNotification = async (args = {}) => {
+  try {
+    const response = await axios.patch(`/notifications/${args.id}`);
+  } catch (error) {
     throw error;
   }
 }
