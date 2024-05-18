b8c956c31a6e2a88dcb2de37728d29f543d80da5
Gonzalo Coda <gcoda@fi.uba.ar>
2024-05-18 16:31:22 -0300

    Fix params from expense endpoint

--- src/app/components/AddExpenseSection.js
+++ src/app/components/AddExpenseSection.js
@@ -22,21 +22,21 @@ export default function AddExpenseSection({ groupId, categories, refreshBills })
     const { addBill, getBills } = useGroupStore();
     const { showSnackbar } = useSnackbar();
   
-    const [newBill, setNewBill] = useState({ billAmount: 0, categoryId: 0 });
+    const [newBill, setNewBill] = useState({ bill_amount: 0, category_id: 0 });
     const [divisionMode, setDivisionMode] = useState("");
   
     const handleAddExpense = async () => {
         const params = {
           ...newBill,
-          groupId: groupId,
+          group_id: groupId,
           mode: divisionMode,
         };
     
-        if (parseInt(params.billAmount) <= 0) {
+        if (parseInt(params.bill_amount) <= 0) {
           showSnackbar('The amount must be greater than 0', 'error');
           return;
         }
-        if (parseInt(params.categoryId) <= 0) {
+        if (parseInt(params.category_id) <= 0) {
           showSnackbar('You must choose a category', 'error');
           return;
         }
@@ -47,6 +47,7 @@ export default function AddExpenseSection({ groupId, categories, refreshBills })
     
         try {
           await addBill(params);
+          setNewBill({ bill_amount: 0, category_id: 0 })
           await refreshBills();
           showSnackbar('The bill was added successfully', 'success');
         } catch (error) {
@@ -67,8 +68,8 @@ export default function AddExpenseSection({ groupId, categories, refreshBills })
         <Box>
           <TextField
             label="Amount"
-            value={newBill.billAmount}
-            onChange={(e) => setNewBill({ ...newBill, billAmount: e.target.value })}
+            value={newBill.bill_amount}
+            onChange={(e) => setNewBill({ ...newBill, bill_amount: e.target.value })}
             type="number"
             fullWidth
             margin="normal"
@@ -76,8 +77,8 @@ export default function AddExpenseSection({ groupId, categories, refreshBills })
           <FormControl fullWidth margin="normal">
             <InputLabel>Category</InputLabel>
             <Select
-              value={newBill.categoryId}
-              onChange={(e) => setNewBill({ ...newBill, categoryId: e.target.value })}
+              value={newBill.category_id}
+              onChange={(e) => setNewBill({ ...newBill, category_id: e.target.value })}
             >
               {categories.map((category) => (
                 <MenuItem key={category.id} value={category.id}>{category.icon + ' ' + category.name}</MenuItem>
