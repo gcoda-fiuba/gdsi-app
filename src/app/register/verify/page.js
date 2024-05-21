5e900f3e069b1c374b906fc831d4e4d50ba73da4
Gonzalo Coda <gcoda@fi.uba.ar>
2024-05-20 21:25:15 -0300

    Fix useSearchParams on verify

--- src/app/register/verify/page.js
+++ src/app/register/verify/page.js
@@ -2,12 +2,11 @@
 
 import { Grid } from '@mui/material';
 import { useSnackbar } from "@/app/context/SnackbarContext";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import useAuthStore from "@/app/store/auth";
 import { useRouter, useSearchParams } from 'next/navigation'
 
-export default function Verify() {
-
+function VerifyContent() {
     const {verifyAccount} = useAuthStore()
     const router = useRouter();
     const searchParams = useSearchParams()
@@ -36,8 +35,16 @@ export default function Verify() {
     }, []);
 
     return (
-      <Grid container alignItems="center" justifyContent="center" style={{ flexDirection: 'column', height: '100vh' }}>
-          {verifying && <h1>Estamos verificando tu cuenta...</h1>}
-      </Grid>
+        <Grid container alignItems="center" justifyContent="center" style={{ flexDirection: 'column', height: '100vh' }}>
+            {verifying && <h1>Estamos verificando tu cuenta...</h1>}
+        </Grid>
+    );
+}
+
+export default function Verify() {
+    return (
+        <Suspense fallback={<div>Cargando...</div>}>
+            <VerifyContent />
+        </Suspense>
     );
 }
