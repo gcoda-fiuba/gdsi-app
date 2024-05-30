1341c5baaf9dfda893caf28ac6b1a045c6cb2c9e
Joaquin Ordoñez <joacoordo@gmail.com>
2024-05-30 17:19:29 -0300

    Making it look nicer

--- src/app/components/AddExpenseSection.js
+++ src/app/components/AddExpenseSection.js
@@ -11,19 +11,17 @@ import {
   MenuItem,
   Button,
   DialogContentText,
-  FormControlLabel,
-  Checkbox,
   Dialog,
   DialogTitle,
   DialogContent,
   DialogActions,
-  Fragment,
   Typography
 } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import useGroupStore from "@/app/store/groups";
 import { useSnackbar } from "@/app/context/SnackbarContext";
 import EmojiPicker from 'emoji-picker-react';
+import Divider from "@mui/material/Divider";
 
 
 
@@ -34,38 +32,39 @@ export default function AddExpenseSection({ groupId, categories, refreshBills })
   
     const [newExpense, setNewExpense] = useState({ bill_amount: 0, category_id: 0, custom_category: '' });
     const [divisionMode, setDivisionMode] = useState("");
-    const [isOpen, setIsOpen] = useState(false);
-    const [customCategoryName, setCustomCategoryName] = useState('');
-    const [categoryEmoji, setCategoryEmoji] = useState('');
 
-    const handleClickOpen = (event) => {
-      setIsOpen(true);
+    const [isCustomCategoryOpen, setIsCustomCategoryOpen] = useState(false);
+    const [customCategoryName, setCustomCategoryName] = useState('');
+    const [customCategoryEmoji, setCustomCategoryEmoji] = useState('');
+
+    const handleClickOpenCustomCategory = () => {
+      setIsCustomCategoryOpen(true);
     }
-    const handleClose = () => {
-        setIsOpen(false);
+    const handleCloseCustomCategory = () => {
+        setCustomCategoryName('');
+        setCustomCategoryEmoji('');
+        setIsCustomCategoryOpen(false);
     };
 
-    const handleSubmit = async (event) => {
+    const handleSubmitCustomCategory = async (event) => {
       try {
         event.preventDefault();
         const params = {
           name: customCategoryName,
-          icon: categoryEmoji,
+          icon: customCategoryEmoji,
           color: ""
         }
-        //console.log("hola2");
         await addCustomCategory(params);
-        //console.log("hola");
         await refreshBills();
-        handleClose();
+        handleCloseCustomCategory();
       } catch (error) {
-        console.log("error desconocido")
-        //showSnackbar(error.response.data.error, 'error');
+        showSnackbar('Hubo un error', 'error');
       }
     }
 
-
-    const handleCategoryNameChange = (event) => setCustomCategoryName(event.target.value);
+    const handleCategoryNameChange = (event) => {
+        setCustomCategoryName(event.target.value);
+    }
 
     const handleAddExpense = async () => {
         const params = {
@@ -101,125 +100,100 @@ export default function AddExpenseSection({ groupId, categories, refreshBills })
 
   return (
     <>
-    <Dialog
-      open={isOpen}
-      onClose={handleClose}
-      PaperProps={{
-          component: 'form',
-          onSubmit: handleSubmit,
-      }}
-      >
-      <DialogTitle color = "secondary">Crea un nuevo grupo</DialogTitle>
-      <DialogContent>
-          <EmojiPicker onEmojiClick={(emojiData) => setCategoryEmoji(emojiData.emoji)}/>
-          <DialogContentText>
-              Elije el nombre de tu categoría personalizada
-          </DialogContentText>
-          <TextField
-              autoFocus
-              required
-              margin="dense"
-              id="name"
-              name="groupName"
-              label="Nombre"
-              type="text"
-              fullWidth
-              variant="standard"
-              onChange={handleCategoryNameChange}
-          />
-          <Typography variant='subtitle1'>Tu cateogría personalizada: {categoryEmoji} {customCategoryName}</Typography>
-      </DialogContent>
-      <DialogActions>
-          <Button variant="outlined" color="secondary" onClick={handleClose}>Cancel</Button>
-          <Button variant="outlined" color="secondary" type="submit">Crear</Button>
-      </DialogActions>
-    </Dialog>
+        <Dialog
+          open={isCustomCategoryOpen}
+          onClose={handleCloseCustomCategory}
+          PaperProps={{
+              component: 'form',
+              onSubmit: handleSubmitCustomCategory,
+          }}
+          >
+            <DialogTitle color = "secondary">New category:</DialogTitle>
+            <Typography variant='subtitle1' style={{ alignSelf: 'center' }}>{customCategoryEmoji} {customCategoryName}</Typography>
+            <Divider />
+            <DialogContent>
+                <TextField
+                    autoFocus
+                    required
+                    margin="dense"
+                    id="name"
+                    name="groupName"
+                    label="Nombre"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    onChange={handleCategoryNameChange}
+                    style={{ marginBottom: '2%' }}
+                />
+                <Accordion>
+                  <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel1-content"
+                      id="panel1-header"
+                  >
+                      {customCategoryEmoji ? `Selected ${customCategoryEmoji}` : 'Select your category emoji'}
+                  </AccordionSummary>
+                  <AccordionDetails>
+                      <EmojiPicker onEmojiClick={(emojiData) => setCustomCategoryEmoji(emojiData.emoji)} previewConfig={{showPreview: false}}/>
+                  </AccordionDetails>
+                </Accordion>
+            </DialogContent>
+            <DialogActions>
+              <Button variant="outlined" color="error" onClick={handleCloseCustomCategory}>Cancel</Button>
+              <Button variant="outlined" color="secondary" type="submit">Crear</Button>
+            </DialogActions>
+        </Dialog>
 
 
-    <Accordion sx={{ marginTop: 2 }}>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="add-expense-content"
-        id="add-expense-header"
-      >
-        <DialogContentText>Add Expense</DialogContentText>
-      </AccordionSummary>
-      <AccordionDetails>
-      <Button variant="outlined" color="secondary" onClick={handleClickOpen} /*style={{margin: '2%'}}*/>
-                Create Custom Category
-            </Button>
-        <Box>
-          <TextField
-            label="Amount"
-            value={newExpense.bill_amount}
-            onChange={(e) => setNewExpense({ ...newExpense, bill_amount: e.target.value })}
-            type="number"
-            fullWidth
-            margin="normal"
-          />
-          <FormControl fullWidth margin="normal">
-            <InputLabel>Category</InputLabel>
-            <Select
-              value={newExpense.category_id}
-              onChange={(e) => setNewExpense({ ...newExpense, category_id: e.target.value })}
-            >
-              {categories.map((category) => (
-                <MenuItem key={category.id} value={category.id}>{category.icon + ' ' + category.name}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+        <Accordion sx={{ marginTop: 2 }}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="add-expense-content"
+            id="add-expense-header"
+          >
+            <DialogContentText>Add Expense</DialogContentText>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Box>
+                <TextField
+                    label="Amount"
+                    value={newExpense.bill_amount}
+                    onChange={(e) => setNewExpense({ ...newExpense, bill_amount: e.target.value })}
+                    type="number"
+                    fullWidth
+                    margin="normal"
+                />
+                <FormControl fullWidth margin="normal">
+                    <InputLabel>Category</InputLabel>
+                    <Select
+                        value={newExpense.category_id}
+                        onChange={(e) => setNewExpense({ ...newExpense, category_id: e.target.value })}
+                    >
+                    {categories.map((category) => (
+                        <MenuItem key={category.id} value={category.id}>{category.icon + ' ' + category.name}</MenuItem>
+                    ))}
+                    </Select>
+                </FormControl>
+                <Button onClick={handleClickOpenCustomCategory} variant="outlined" color="secondary" fullWidth>
+                    Create Custom Category
+                </Button>
 
+                <FormControl fullWidth margin="normal">
+                    <InputLabel>Division Mode</InputLabel>
+                    <Select
+                        value={divisionMode}
+                        onChange={(e) => setDivisionMode(e.target.value)}
+                    >
+                        <MenuItem value="equitative">Equitative</MenuItem>
+                    </Select>
+                </FormControl>
 
-          <FormControl fullWidth margin="normal">
-            <InputLabel>Division Mode</InputLabel>
-            <Select
-              value={divisionMode}
-              onChange={(e) => setDivisionMode(e.target.value)}
-            >
-              <MenuItem value="equitative">Equitative</MenuItem>
-            </Select>
-          </FormControl>
-          <Button onClick={handleAddExpense} variant="outlined" color="secondary" fullWidth>
-            Add Expense
-          </Button>
-        </Box>
-      </AccordionDetails>
-    </Accordion>
+                <Button onClick={handleAddExpense} variant="outlined" color="secondary" fullWidth>
+                    Add Expense
+                </Button>
+            </Box>
+          </AccordionDetails>
+        </Accordion>
     </>
   );
 }
-
-
-
-/*
-      <Dialog
-        open={isOpen}
-        onClose={handleClose}
-        PaperProps={{
-            component: 'form',
-            onSubmit: handleSubmit,
-        }}
-      >
-        <DialogTitle color = "secondary">Crea un nuevo grupo</DialogTitle>
-        <DialogContent>
-            <DialogContentText>
-                ¡Se creativo! Este va a ser el nombre del grupo.
-            </DialogContentText>
-            <TextField
-                autoFocus
-                required
-                margin="dense"
-                id="name"
-                name="groupName"
-                label="Nombre"
-                type="text"
-                fullWidth
-                variant="standard"
-                onChange={handleNameChange}
-            />
-        </DialogContent>
-        <DialogActions>
-            <Button variant="outlined" color="secondary" onClick={handleClose}>Cancel</Button>
-            <Button variant="outlined" color="secondary" type="submit">Crear</Button>
-        </DialogActions>
-      </Dialog> */
