8e5af5569ed2ba094c99a82a3ac163e9a5c279ee
joac001 <jordonez@fi.uba.ar>
2024-06-17 18:21:13 -0300

    fixing payument modal slider

--- src/app/components/PaymentModal.js
+++ src/app/components/PaymentModal.js
@@ -1,22 +1,39 @@
 'use client';
 
-import {Dialog, DialogContent, DialogActions, Button, Typography, DialogTitle, Slider} from "@mui/material";
+import {
+    Dialog,
+    DialogContent,
+    DialogActions,
+    Button,
+    Typography,
+    DialogTitle,
+    OutlinedInput,
+    FormControl, InputLabel
+} from "@mui/material";
 import useGroupStore from "@/app/store/groups";
 import {useSnackbar} from "@/app/context/SnackbarContext";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function PaymentModal({ debt, open, onClose, refreshDebts }) {
     const { patchBill } = useGroupStore();
     const { showSnackbar } = useSnackbar();
-    const [value, setValue] = useState(0);
+    const [amountToPay, setAmountToPay] = useState(debt.amount);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
+    useEffect(() => {
+        setAmountToPay(debt.amount);
+    }, []);
+
+    const handleChangeAmountToPay = (event) => {
+        setAmountToPay(event.target.value)
       };
       
-    const handlePayBill = async (debt, amount) => {
+    const handlePayBill = async (debt) => {
+        if (!amountToPay || amountToPay <= 0 || amountToPay > debt.amount) {
+            showSnackbar('Invalid amount', 'error');
+            return;
+        }
         try {
-            await patchBill(debt.id, {amount: amount});
+            await patchBill(debt.id, {amount: amountToPay});
             showSnackbar('Payment registered');
             refreshDebts();
             onClose();
@@ -29,28 +46,26 @@ export default function PaymentModal({ debt, open, onClose, refreshDebts }) {
     return (
         <Dialog open={open} onClose={onClose}>
             <DialogTitle>Pay debt</DialogTitle>
-
                 <DialogContent>
                     <Typography variant="subtitle2">Pay to</Typography>
                     <Typography>{debt.user}</Typography>
-                    <Typography variant="subtitle2">Amount</Typography>
-                    <Typography>${debt.amount}</Typography>
-                    <Typography variant="subtitle2">Or make a partial payment instead</Typography>
-                    <Slider
-                        value={value}
-                        onChange={handleChange}
-                        aria-label="Partial pay"
-                        valueLabelDisplay="auto"
-                        step={1}
-                        min={0}
-                        max={debt.amount}
-                    />
+                    {/*<Typography variant="subtitle2">{`${amountToPay}`}</Typography>*/}
+                    <FormControl fullWidth variant="outlined" style={{ marginTop: 15 }}>
+                        <InputLabel htmlFor="outlined-adornment-number">Amount to pay</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-number"
+                            type="number"
+                            value={amountToPay}
+                            onChange={handleChangeAmountToPay}
+                            label="Amount to pay"
+                            autoFocus={true}
+                        />
+                    </FormControl>
                 </DialogContent>
 
             <DialogActions>
                 <Button variant="outlined" color="error" onClick={onClose}>Cancel</Button>
                 <Button variant="outlined" color="secondary" onClick={() => handlePayBill(debt, debt.amount)}>Pay</Button>
-                <Button variant="outlined" color="secondary" onClick={() => handlePayBill(debt, value)}>Partial Pay</Button>
             </DialogActions>
         </Dialog>
     );
